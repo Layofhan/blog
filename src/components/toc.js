40395ccba8db5b9af92c429595d05dd83a6b4ce8
xugh <xugh@fingard.com>
2025-06-15 21:26:54 +0800

    层级修改

--- src/components/toc.js
+++ src/components/toc.js
@@ -37,7 +37,7 @@ export default function TableofContent({ heading }) {
       <Link
         data-level={heading.level}
         href={`#${heading.text}`}
-        className={`"data-[level=two]:ml-1 data-[level=three]:ml-2 leading-9 rounded-lg px-3 py-1 font-normal text-zinc-500 dark:text-zinc-300 hover:bg-zinc-50 hover:text-zinc-900 dark:hover:text-zinc-50 dark:hover:bg-zinc-800 transition duration-400"  ${
+        className={`"data-[level=one]:ml-1 data-[level=two]:ml-2 data-[level=three]:ml-3 leading-9 rounded-lg px-3 py-1 font-normal text-zinc-500 dark:text-zinc-300 hover:bg-zinc-50 hover:text-zinc-900 dark:hover:text-zinc-50 dark:hover:bg-zinc-800 transition duration-400"  ${
           highlighted &&
           "bg-zinc-50 text-zinc-900 dark:text-zinc-100 dark:bg-zinc-800"
         }`}
